8ff43a5225d04027d78f569e64fdcaeaa4c85642
Matthew Taylor <seleukos1209@googlemail.com>
2026-03-24 11:02:42 +0000

    updated initial comment from generic shiny app comment

--- app.R
+++ app.R
@@ -1,11 +1,10 @@
 #
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    https://shiny.posit.co/
-#
+# This is a shiny application for running bulk RNAseq and associated downstream
+# analyses (GO, GSEA, GAGE). Includes essential QC such as PCA, hierarchical
+# clustering/heatmaps, library size plots and count density plots. Also included
+# is the ability to explore specific gene sets (boxplots, volcanos etc), and
+# compare different contrasts. Differetial gene expression handled using 
+# DESeq2.
 
 required_packages <- c("limma",
                        
